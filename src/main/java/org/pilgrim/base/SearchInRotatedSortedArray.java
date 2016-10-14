0e5304c8bfa3c5e9bb3b77c5b6787657d345c224
Sergiy Goncharenko <sergeyltd@gmail.com>
2016-10-14 13:40:31 -0700

    update

--- src/main/java/org/pilgrim/base/SearchInRotatedSortedArray.java
+++ src/main/java/org/pilgrim/base/SearchInRotatedSortedArray.java
@@ -1,30 +1,43 @@
 package org.pilgrim.base;
 
-public class SearchInRotatedSortedArray {
-	public int search(int[] nums, int target) {
-	    int left = 0;
-	    int right= nums.length-1;
-	 
-	    while(left<=right){
-	        int mid = left + (right-left)/2;
-	        if(target==nums[mid])
-	            return mid;
-	 
-	        if(nums[left]<=nums[mid]){
-	            if(nums[left]<=target&& target<nums[mid]){
-	                right=mid-1;
-	            }else{
-	                left=mid+1;
-	            }
-	        }else{
-	            if(nums[mid]<target&& target<=nums[right]){
-	                left=mid+1;
-	            }else{
-	                right=mid-1;
-	            }
-	        }    
-	    }
-	 
-	    return -1;
-	}
+public class SearchInRotatedSortedArray
+{
+    public int search(int[] a,
+                      int target)
+    {
+        int i = 0;
+        int j = a.length - 1;
+
+        while (i <= j)
+        {
+            int mid = (j + i) / 2;
+            if (target == a[mid])
+                return mid;
+
+            if (a[i] <= a[mid])
+            {
+                if (a[i] <= target && target < a[mid])
+                {
+                    j = mid - 1;
+                }
+                else
+                {
+                    i = mid + 1;
+                }
+            }
+            else
+            {
+                if (a[mid] < target && target <= a[j])
+                {
+                    i = mid + 1;
+                }
+                else
+                {
+                    j = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
 }
