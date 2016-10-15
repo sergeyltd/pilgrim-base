b0166134f4a82d7df10ce6da8f3bdb8e1bdfe313
sergeyg <sergeyg@EDXLT8181.corp.edifecs.com>
2016-10-15 13:19:13 -0700

    update

--- src/main/java/org/pilgrim/base/LongestIncreasingSubsequence.java
+++ src/main/java/org/pilgrim/base/LongestIncreasingSubsequence.java
@@ -1,29 +1,33 @@
 package org.pilgrim.base;
 
-public class LongestIncreasingSubsequence {
-	public int lengthOfLIS(int arr[]) {
-		return lengthOfLIS(arr, arr.length);
-	}
+public class LongestIncreasingSubsequence
+{
+    static public int lengthOfLIS(int arr[])
+    {
+        return lengthOfLIS(arr, arr.length);
+    }
 
-	public int lengthOfLIS(int a[], int n) {
-		int lis[] = new int[n];
-		int i, j, max = 0;
+    static public int lengthOfLIS(int a[],
+                                  int n)
+    {
+        int lis[] = new int[n];
+        int i, j, max = 0;
 
-		/* Initialize LIS values for all indexes */
-		for (i = 0; i < n; i++)
-			lis[i] = 1;
+        /* Initialize LIS values for all indexes */
+        for (i = 0; i < n; i++)
+            lis[i] = 1;
 
-		/* Compute optimized LIS values in bottom up manner */
-		for (i = 1; i < n; i++)
-			for (j = 0; j < i; j++)
-				if (a[i] > a[j] && lis[i] < lis[j] + 1)
-					lis[i] = lis[j] + 1;
+        /* Compute optimized LIS values in bottom up manner */
+        for (i = 1; i < n; i++)
+            for (j = 0; j < i; j++)
+                if (a[i] > a[j] && lis[i] < lis[j] + 1)
+                    lis[i] = lis[j] + 1;
 
-		/* Pick maximum of all LIS values */
-		for (i = 0; i < n; i++)
-			if (max < lis[i])
-				max = lis[i];
+        /* Pick maximum of all LIS values */
+        for (i = 0; i < n; i++)
+            if (max < lis[i])
+                max = lis[i];
 
-		return max;
-	}
+        return max;
+    }
 }
