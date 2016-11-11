f507de9208a1d9129a3d7e49dfb7e193637f5d7d
Sergiy Go <sergeyltd@gmail.com>
2016-11-11 09:29:07 -0800

    update

--- src/main/java/PercolationStats.java
+++ src/main/java/PercolationStats.java
@@ -4,73 +4,73 @@ import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 
-	private double[] thresholds;
-	private Percolation p;
-	private int t;
+    private double[] thresholds;
+    private Percolation p;
+    private int t;
 
-	/**
-	 * Constructor for PercolationStats
-	 */
-	public PercolationStats(int N, int T) {
-		t = T;
-		if (N <= 0 || T <= 0)
-			throw new IllegalArgumentException();
-		thresholds = new double[T];
+    /**
+     * Constructor for PercolationStats
+     */
+    public PercolationStats(int N, int T) {
+        t = T;
+        if (N <= 0 || T <= 0)
+            throw new IllegalArgumentException();
+        thresholds = new double[T];
 
-		for (int i = 0; i < T; i++) {
-			p = new Percolation(N);
-			int count = 0;
-			while (!p.percolates()) {
-				int x = StdRandom.uniform(N) + 1;
-				int y = StdRandom.uniform(N) + 1;
-				if (!p.isOpen(x, y)) {
-					p.open(x, y);
-					count++;
-				}
-			}
-			thresholds[i] = Integer.valueOf(count).doubleValue() / (N * N);
-		}
-	}
+        for (int i = 0; i < T; i++) {
+            p = new Percolation(N);
+            int count = 0;
+            while (!p.percolates()) {
+                int x = StdRandom.uniform(N) + 1;
+                int y = StdRandom.uniform(N) + 1;
+                if (!p.isOpen(x, y)) {
+                    p.open(x, y);
+                    count++;
+                }
+            }
+            thresholds[i] = Integer.valueOf(count).doubleValue() / (N * N);
+        }
+    }
 
-	public double confidenceLo() {
-		double lowConf = mean() - (1.96 * stddev() / Math.sqrt(t));
-		return lowConf;
-	}
+    public double confidenceLo() {
+        double lowConf = mean() - (1.96 * stddev() / Math.sqrt(t));
+        return lowConf;
+    }
 
-	public double confidenceHi() {
-		double highConf = mean() + (1.96 * stddev() / Math.sqrt(t));
-		return highConf;
-	}
+    public double confidenceHi() {
+        double highConf = mean() + (1.96 * stddev() / Math.sqrt(t));
+        return highConf;
+    }
 
-	/**
-	 * Returns the sample mean
-	 */
-	public double mean() {
-		return StdStats.mean(thresholds);
-	}
+    /**
+     * Returns the sample mean
+     */
+    public double mean() {
+        return StdStats.mean(thresholds);
+    }
 
-	/**
-	 * Returns the sample standard deviation
-	 */
-	public double stddev() {
-		return StdStats.stddev(thresholds);
-	}
+    /**
+     * Returns the sample standard deviation
+     */
+    public double stddev() {
+        return StdStats.stddev(thresholds);
+    }
 
-	public static void main(String[] args) {
-		Out out = new Out();
-		int N = Integer.parseInt(args[0]);
-		int T = Integer.parseInt(args[1]);
+    public static void main(String[] args) {
+        Out out = new Out();
+        int N = Integer.parseInt(args[0]);
+        int T = Integer.parseInt(args[1]);
 
-		PercolationStats ps = new PercolationStats(N, T);
+        PercolationStats ps = new PercolationStats(N, T);
 
-		double mean = ps.mean();
-		double stddev = ps.stddev();
-		double lowConf = ps.confidenceLo();
-		double highConf = ps.confidenceLo();
+        double mean = ps.mean();
+        double stddev = ps.stddev();
+        double lowConf = ps.confidenceLo();
+        double highConf = ps.confidenceLo();
 
-		out.printf("mean                    = %f\n", mean);
-		out.printf("stddev                  = %f\n", stddev);
-		out.printf("95%% confidence interval = %f, %f\n", lowConf, highConf);
-		out.close();
-	}
+        out.printf("mean                    = %f\n", mean);
+        out.printf("stddev                  = %f\n", stddev);
+        out.printf("95%% confidence interval = %f, %f\n", lowConf, highConf);
+        out.close();
+    }
 }
