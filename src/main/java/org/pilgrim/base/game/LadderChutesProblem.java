a9d74cf097b1582647199c4b0a68131d654d5e70
Sergiy Goncharenko <sergeyltd@gmail.com>
2016-11-14 18:02:14 -0800

    add description

--- src/main/java/org/pilgrim/base/game/LadderChutesProblem.java
+++ src/main/java/org/pilgrim/base/game/LadderChutesProblem.java
@@ -1,3 +1,31 @@
+/**
+ * Snake and Ladder Problem
+Given a snake and ladder board, find the minimum number of dice throws required to reach
+the destination or last cell from source or 1st cell. Basically, the player has total control
+over outcome of dice throw and wants to find out minimum number of throws required to reach last cell.
+
+If the player reaches a cell which is base of a ladder, the player has to climb up that
+ladder and if reaches a cell is mouth of the snake, has to go down to the tail of snake without a dice throw.
+
+snakesladders
+
+For example consider the board shown on right side (taken from here),
+the minimum number of dice throws required to reach cell 30 from cell 1 is 3. Following are steps.
+
+a) First throw two on dice to reach cell number 3 and then ladder to reach 22
+b) Then throw 6 to reach 28.
+c) Finally through 2 to reach 30.
+
+There can be other solutions as well like (2, 2, 6), (2, 4, 4), (2, 3, 5).. etc.
+
+We strongly recommend to minimize the browser and try this yourself first.
+The idea is to consider the given snake and ladder board as a directed graph with number
+of vertices equal to the number of cells in the board. The problem reduces to finding the shortest
+path in a graph. Every vertex of the graph has an edge to next six vertices
+if next 6 vertices do not have a snake or ladder. If any of the next six vertices has a snake or ladder,
+then the edge from current vertex goes to the top of the ladder or tail of the snake.
+Since all edges are of equal weight, we can efficiently find shortest path using Breadth First Search of the graph.
+ */
 package org.pilgrim.base.game;
 
 import java.util.Arrays;
