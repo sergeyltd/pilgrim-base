6efefb95db51c87621559fb29b78e6d00ca88017
sergeyg <sergeyg@EDXLT8181.corp.edifecs.com>
2016-12-02 19:22:13 -0800

    add

---
+++ src/main/java/org/pilgrim/base/LFUCache.java
@@ -0,0 +1,22 @@
+package org.pilgrim.base;
+public class LFUCache {
+
+    public LFUCache(int capacity) {
+        
+    }
+    
+    public int get(int key) {
+        return -1;
+    }
+    
+    public void set(int key, int value) {
+        
+    }
+}
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.set(key,value);
+ */
