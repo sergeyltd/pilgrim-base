72563fbf8fd3279a7333aa95a6ccadd2115ee671
Sergiy Goncharenko <sergeyltd@gmail.com>
2017-01-05 13:25:39 -0800

    add is Valid BST

--- src/main/java/org/pilgrim/base/BSTree.java
+++ src/main/java/org/pilgrim/base/BSTree.java
@@ -244,6 +244,24 @@ public class BSTree
             q = ql;
         }
     }
+    
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+    }
+    
+    public boolean isValidBST(TreeNode root, double min, double max) {
+        if (null==root)
+        {
+            return true;
+        }
+        
+        if (root.val <= min || max <= root.val)
+        {
+            return false;
+        }
+        
+        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+    }
 
     // Driver program to test above functions
     public static void main(String[] args)
