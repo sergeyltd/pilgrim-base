dabb0cbf591ca7654e2cf3771795d3d37fbbb338
sergeyltd@gmail.com <sergeylyd@gmail.com>
2021-04-27 22:35:29 -0700

    add MissingNumber

---
+++ src/main/java/org/pilgrim/leetcode/y2021/MissingNumber.java
@@ -0,0 +1,66 @@
+package org.pilgrim.leetcode.y2021;
+
+/*Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+
+ 
+
+Example 1:
+
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+Example 2:
+
+Input: nums = [0,1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+Example 3:
+
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+Example 4:
+
+Input: nums = [0]
+Output: 1
+Explanation: n = 1 since there is 1 number, so all numbers are in the range [0,1]. 1 is the missing number in the range since it does not appear in nums.
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 104
+0 <= nums[i] <= n
+All the numbers of nums are unique.*/
+
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+        if(nums == null){
+            return -1;
+        }
+        
+        int n = nums.length;
+        
+        //sort
+        for(int i=0; i<n; i++){
+            int k = nums[i];
+            
+            while(k != i && k != n){
+                nums[i] = nums[k];
+                nums[k] = k;
+                k = nums[i];
+            }
+        }
+        
+        for(int i=0; i<n; i++){
+            int k = nums[i];
+            if(k == n){
+                return i;
+            }
+        }
+        
+        return n;
+    }
+}
