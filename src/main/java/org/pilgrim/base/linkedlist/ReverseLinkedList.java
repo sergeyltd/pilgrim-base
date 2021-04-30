a7e5968f7b859b4b41a5256f056821e83e3562a1
sergeyltd@gmail.com <sergeylyd@gmail.com>
2021-04-29 22:31:55 -0700

    IntersectionTwoLinkedLists

--- src/main/java/org/pilgrim/base/linkedlist/ReverseLinkedList.java
+++ src/main/java/org/pilgrim/base/linkedlist/ReverseLinkedList.java
@@ -139,8 +139,8 @@ public class ReverseLinkedList {
     }
 
     public static class ListNode {
-        int val;
-        ListNode next;
+        public int val;
+        public ListNode next;
 
         ListNode(int x) {
             val = x;
