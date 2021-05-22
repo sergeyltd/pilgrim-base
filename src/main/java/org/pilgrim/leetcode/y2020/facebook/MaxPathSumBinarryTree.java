4a682899a86c681e55e620d6aff5be032baf17cf
sergeyltd@gmail.com <sergeylyd@gmail.com>
2021-05-21 23:25:38 -0700

    add AddOneRowToTree

--- src/main/java/org/pilgrim/leetcode/y2020/facebook/MaxPathSumBinarryTree.java
+++ src/main/java/org/pilgrim/leetcode/y2020/facebook/MaxPathSumBinarryTree.java
@@ -1,36 +1,47 @@
 package org.pilgrim.leetcode.y2020.facebook;
 
 public class MaxPathSumBinarryTree {
-    
-    public static class TreeNode{
+
+    public static class TreeNode {
         public int val;
         public TreeNode left;
         public TreeNode right;
-        TreeNode(){}
-        TreeNode(int val) {this.val = val;}
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
     }
-    
+
     int maxSum = Integer.MIN_VALUE;
-    
+
     public int getMaxSumTreePath(TreeNode node) {
         maxSumHelper(node);
-        
+
         return maxSum;
     }
-    
+
     private int maxSumHelper(TreeNode node) {
-        if(node == null) {
+        if (node == null) {
             return 0;
         }
-        
+
         int leftMax = Math.max(maxSumHelper(node.left), 0);
         int rightMax = Math.max(maxSumHelper(node.right), 0);
-        
+
         int newMax = node.val + leftMax + rightMax;
         maxSum = Math.max(newMax, maxSum);
-        
+
         return node.val + Math.max(leftMax, rightMax);
-        
+
     }
 
     public static void main(String[] args) {
