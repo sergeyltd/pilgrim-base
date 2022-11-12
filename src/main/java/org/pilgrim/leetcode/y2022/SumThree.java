cc4f6824c260ae34d3aacc1c5511a54c7a78dd3a
sergeyltd <sergeyltd@gmail.com>
2022-11-12 14:57:20 -0800

    Update SumThree.java

--- src/main/java/org/pilgrim/leetcode/y2022/SumThree.java
+++ src/main/java/org/pilgrim/leetcode/y2022/SumThree.java
@@ -6,7 +6,89 @@ public class SumThree {
 		// TODO Auto-generated method stub
 
 	}
+	
+    public List<List<Integer>> threeSum(int[] nums) {
+        int target = 0;
+        List<List<Integer>> res = new ArrayList<>();
+        
+        Arrays.sort(nums);
+        
+        for(int i=0; i<nums.length-2; i++){
+            int a = nums[i];
+            
+            target = -a;
+            
+            List<List<Integer>> list = twoSum(nums, i+1, target);
+            
+            while(a == nums[i+1] && i<nums.length-3){
+                i++;
+            }
+            
+            if(list == null || list.isEmpty()){
+                continue;
+            }
+            
+            res.addAll(list);
+        }
+        
+        Iterator<List<Integer>> it = res.iterator();
+        List<Integer> node = null;
+        if(it.hasNext()) {
+            node = it.next();
+        }
+        while (it.hasNext()) {
+            List<Integer> item = it.next();
+            if(node.get(0).equals(item.get(0))
+               && node.get(1).equals(item.get(1))
+               && node.get(2).equals(item.get(2)))
+            {
+                it.remove();
+            }
+            else{
+                node = item;
+            }
+        }
+        
+        return res;
+    }
+    
+    private List<List<Integer>> twoSum(int[] arr, int start, int target){
+        List<List<Integer>> lists = new ArrayList<>();
+        
+        // Traverse array from both sides
+        int i = start;
+        int j = arr.length - 1;
+        while(i < j){
+            int s = arr[i] + arr[j];
+            if(target == s){
+                List<Integer> list = new ArrayList<>();
+                list.add(-target);
+                list.add(arr[i]);
+                list.add(arr[j]);
+                
+                Collections.sort(list);
 
+                lists.add(list);
+                
+                i++;
+                j--;
+            }
+            else if(target > s){
+                i++;
+                while(i < j && arr[i] == arr[i+1] && arr[i]*2 != target){
+                    i++;
+                }
+            }
+            else if(target < s){
+                j--;
+                while(i < j && arr[j] == arr[j - 1] && arr[j]*2 != target){
+                    j--;
+                }
+            }
+        }
+        
+        return lists;
+    }
 }
 
 /**
