7011d36d54e0273c961c62cee734434f34b935de
sergeyltd <sergeyltd@gmail.com>
2022-11-12 14:55:49 -0800

    Create ThreeSumFast.java

---
+++ src/main/java/org/pilgrim/leetcode/y2022/ThreeSumFast.java
@@ -0,0 +1,69 @@
+package org.pilgrim.leetcode.y2022;
+
+public class ThreeSumFast {
+
+    // Do not instantiate.
+    private ThreeSumFast() { }
+
+    // returns true if the sorted array a[] contains any duplicated integers
+    private static boolean containsDuplicates(int[] a) {
+        for (int i = 1; i < a.length; i++){
+          if (a[i] == a[i-1]){
+            return true;
+          }
+        }
+      
+        return false;
+    }
+
+    /**
+     * Prints to standard output the (i, j, k) with {@code i < j < k}
+     * such that {@code a[i] + a[j] + a[k] == 0}.
+     *
+     * @param a the array of integers
+     * @throws IllegalArgumentException if the array contains duplicate integers
+     */
+    public static void printAll(int[] a) {
+        int n = a.length;
+        Arrays.sort(a);
+        if (containsDuplicates(a)) throw new IllegalArgumentException("array contains duplicate integers");
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                int k = Arrays.binarySearch(a, -(a[i] + a[j]));
+                if (k > j) StdOut.println(a[i] + " " + a[j] + " " + a[k]);
+            }
+        }
+    }
+
+    /**
+     * Returns the number of triples (i, j, k) with {@code i < j < k}
+     * such that {@code a[i] + a[j] + a[k] == 0}.
+     *
+     * @param a the array of integers
+     * @return the number of triples (i, j, k) with {@code i < j < k}
+     * such that {@code a[i] + a[j] + a[k] == 0}
+     */
+    public static int count(int[] a) {
+        int n = a.length;
+        Arrays.sort(a);
+        if (containsDuplicates(a)) throw new IllegalArgumentException("array contains duplicate integers");
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i+1; j < n; j++) {
+                int k = Arrays.binarySearch(a, -(a[i] + a[j]));
+                if (k > j) count++;
+            }
+        }
+        return count;
+    }
+
+    /**
+     * Reads in a sequence of distinct integers from a file, specified as a command-line argument;
+     * counts the number of triples sum to exactly zero; prints out the time to perform
+     * the computation.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args)  {
+    }
+}
